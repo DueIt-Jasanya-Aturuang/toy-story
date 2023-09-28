d10007f643c5d30dc3e43701cc242c8088607939
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-09-28 21:26:28 +0700

    fix table payment method

--- infra/migrations/000003_create_table_create_m_payment_methods_table.up.sql
+++ infra/migrations/000003_create_table_create_m_payment_methods_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE m_payment_methods
 (
     id          VARCHAR(64) NOT NULL UNIQUE PRIMARY KEY,
+    profile_id  VARCHAR(64),
     name        VARCHAR(128),
     description TEXT,
     image       VARCHAR(255) default 'default-icon.png',
@@ -9,5 +10,10 @@ CREATE TABLE m_payment_methods
     updated_at  DECIMAL     NOT NULL,
     updated_by  VARCHAR(64),
     deleted_at  DECIMAL,
-    deleted_by  VARCHAR(64)
+    deleted_by  VARCHAR(64),
+    CONSTRAINT fk_m_profile
+        FOREIGN KEY (profile_id)
+            REFERENCES m_profiles(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
